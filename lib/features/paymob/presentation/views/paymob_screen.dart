bd884962382b112581927102fb8dac98935e0233
omarali <omaraliabdelaziz250@gmail.com>
2025-02-12 13:17:09 +0200

    paymob

---
+++ lib/features/paymob/presentation/views/paymob_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class RegisterScreenForPaymob extends StatelessWidget {
+  const RegisterScreenForPaymob({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
