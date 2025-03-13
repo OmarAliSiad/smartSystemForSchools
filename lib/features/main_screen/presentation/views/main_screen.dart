ac1c8934408d720e209028be92e4870441f6dbf6
omarali <omaraliabdelaziz250@gmail.com>
2025-03-13 14:42:33 +0200

    main_screen

--- lib/features/main_screen/presentation/views/main_screen.dart
+++ lib/features/main_screen/presentation/views/main_screen.dart
@@ -4,8 +4,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:smartsystemforschools/features/Attendance/presentation/views/attendance_view.dart';
 import 'package:smartsystemforschools/features/payment/presentation/views/payment_view.dart';
 import 'package:smartsystemforschools/features/settings_view/presentation/manager/themeMode/theme_mode_cubit.dart';
-import 'package:smartsystemforschools/features/tracking/presentation/views/tracking_view.dart';
-import '../../../../core/utils/app_styles.dart';
 import '../../../../core/utils/assets.dart';
 import '../../../../generated/locale_keys.g.dart';
 import '../../../family/presentation/views/family_view.dart';
@@ -19,7 +17,7 @@ class MainScreen extends StatefulWidget {
   State<MainScreen> createState() => _MainScreenState();
 }
 
-class _MainScreenState extends State<MainScreen> {
+class _MainScreenState extends State<MainScreen> with TickerProviderStateMixin {
   int currentPage = 0;
   final List<Widget> screens = [
     HomeView(key: UniqueKey()),
@@ -31,6 +29,48 @@ class _MainScreenState extends State<MainScreen> {
     //     initialLocations: const [],
     //     key: UniqueKey()),
   ];
+
+  // Animation controllers for the tab indicators
+  late List<AnimationController> _animationControllers;
+  late List<Animation<double>> _animations;
+
+  @override
+  void initState() {
+    super.initState();
+    _initAnimations();
+  }
+
+  void _initAnimations() {
+    // Initialize animation controllers for each tab
+    _animationControllers = List.generate(
+      4,
+      (index) => AnimationController(
+        vsync: this,
+        duration: const Duration(milliseconds: 400),
+      ),
+    );
+
+    // Initialize animations for each tab
+    _animations = _animationControllers.map((controller) {
+      return Tween<double>(begin: 0.0, end: 1.0).animate(
+        CurvedAnimation(
+          parent: controller,
+          curve: Curves.easeInOut,
+        ),
+      );
+    }).toList();
+
+    // Start animation for the initial tab
+    _animationControllers[currentPage].forward();
+  }
+
+  @override
+  void dispose() {
+    for (var controller in _animationControllers) {
+      controller.dispose();
+    }
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -44,97 +84,166 @@ class _MainScreenState extends State<MainScreen> {
             index: currentPage,
             children: screens,
           ),
-          bottomNavigationBar: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Divider(
-                thickness: 1,
-                color: themeMode == ThemeMode.dark
-                    ? Colors.white.withOpacity(.50)
-                    : Colors.black.withOpacity(.30),
-              ),
-              BottomNavigationBar(
-                landscapeLayout: BottomNavigationBarLandscapeLayout.spread,
-                selectedLabelStyle:
-                    AppStyles.styleMedium16().copyWith(fontSize: 14),
-                elevation: 0,
-                backgroundColor:
-                    themeMode == ThemeMode.dark ? Colors.black : Colors.white,
-                selectedIconTheme:
-                    const IconThemeData(color: Color(0xff1A0F91)),
-                iconSize: 24,
-                type: BottomNavigationBarType.fixed,
-                currentIndex: currentPage,
-                selectedItemColor: const Color(0xff1A0F91),
-                onTap: (i) {
-                  setState(() {
-                    currentPage = i;
-                  });
-                },
-                items: [
-                  BottomNavigationBarItem(
-                    icon: Image.asset(
-                      Assets.imagesHome,
-                      color: (currentPage == 0)
-                          ? const Color(0xff1A0F91)
-                          : Colors.grey,
-                      width: 24.5,
-                      height: 24.5,
-                    ),
-                    label: LocaleKeys.bottomNavigationBar_home.tr(),
-                  ),
-                  BottomNavigationBarItem(
-                    icon: Image.asset(
-                      color: (currentPage == 1)
-                          ? const Color(0xff1A0F91)
-                          : Colors.grey,
-                      Assets.imagesFamily,
-                      width: 24.5,
-                      height: 24.5,
-                    ),
-                    label: LocaleKeys.bottomNavigationBar_family.tr(),
-                  ),
-                  BottomNavigationBarItem(
-                    icon: Image.asset(
-                      color: (currentPage == 2)
-                          ? const Color(0xff1A0F91)
-                          : Colors.grey,
-                      Assets.imagesAttendence,
-                      width: 24.5,
-                      height: 24.5,
-                    ),
-                    label: LocaleKeys.bottomNavigationBar_attendance.tr(),
-                  ),
-                  BottomNavigationBarItem(
-                    icon: Image.asset(
-                      color: (currentPage == 3)
-                          ? const Color(0xff1A0F91)
-                          : Colors.grey,
-                      Assets.imagesWallet,
-                      width: 24.5,
-                      height: 24.5,
-                    ),
-                    label: LocaleKeys.bottomNavigationBar_wallet.tr(),
-                  ),
-                  // BottomNavigationBarItem(
-                  //   icon: Icon(
-                  //     Icons.location_on,
-                  //     color: (currentPage == 4)
-                  //         ? const Color(0xff1A0F91)
-                  //         : Colors.grey,
-                  //     size: 24.5,
-                  //   ),
-                  //   label: LocaleKeys.tracking_bottomNavLabel.tr(),
-                  // ),
-                ],
-              ),
-              const SizedBox(
-                height: 4,
-              ),
-            ],
-          ),
+          bottomNavigationBar: _buildAnimatedBottomNavigationBar(themeMode),
         );
       },
+    );
+  }
+
+  Widget _buildAnimatedBottomNavigationBar(ThemeMode themeMode) {
+    final isDarkMode = themeMode == ThemeMode.dark;
+    final backgroundColor = isDarkMode ? Colors.black : Colors.white;
+    const primaryColor = Color(0xff1A0F91);
+    final secondaryColor = isDarkMode ? Colors.white70 : Colors.grey;
+    final shadowColor = isDarkMode ? Colors.white12 : Colors.black12;
+
+    return Container(
+      height: 80,
+      decoration: BoxDecoration(
+        color: backgroundColor,
+        boxShadow: [
+          BoxShadow(
+            color: shadowColor,
+            blurRadius: 10,
+            offset: const Offset(0, -2),
+          ),
+        ],
+      ),
+      child: Column(
+        children: [
+          Container(
+            height: 2,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  primaryColor.withOpacity(0.1),
+                  primaryColor,
+                  primaryColor.withOpacity(0.1),
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                _buildNavItem(
+                  0,
+                  Assets.imagesHome,
+                  LocaleKeys.bottomNavigationBar_home.tr(),
+                  primaryColor,
+                  secondaryColor,
+                ),
+                _buildNavItem(
+                  1,
+                  Assets.imagesFamily,
+                  LocaleKeys.bottomNavigationBar_family.tr(),
+                  primaryColor,
+                  secondaryColor,
+                ),
+                _buildNavItem(
+                  2,
+                  Assets.imagesAttendence,
+                  LocaleKeys.bottomNavigationBar_attendance.tr(),
+                  primaryColor,
+                  secondaryColor,
+                ),
+                _buildNavItem(
+                  3,
+                  Assets.imagesWallet,
+                  LocaleKeys.bottomNavigationBar_wallet.tr(),
+                  primaryColor,
+                  secondaryColor,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildNavItem(
+    int index,
+    String iconPath,
+    String label,
+    Color primaryColor,
+    Color secondaryColor,
+  ) {
+    final isSelected = currentPage == index;
+
+    return GestureDetector(
+      onTap: () {
+        if (currentPage != index) {
+          setState(() {
+            // Reset previous animation
+            _animationControllers[currentPage].reverse();
+            currentPage = index;
+            // Start new animation
+            _animationControllers[currentPage].forward();
+          });
+        }
+      },
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 300),
+        width: 70,
+        decoration: BoxDecoration(
+          color:
+              isSelected ? primaryColor.withOpacity(0.1) : Colors.transparent,
+          borderRadius: BorderRadius.circular(15),
+        ),
+        padding: const EdgeInsets.symmetric(vertical: 8),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            AnimatedBuilder(
+              animation: _animations[index],
+              builder: (context, child) {
+                return Transform.scale(
+                  scale:
+                      isSelected ? 1.0 + (_animations[index].value * 0.2) : 1.0,
+                  child: Image.asset(
+                    iconPath,
+                    color: isSelected ? primaryColor : secondaryColor,
+                    width: 24,
+                    height: 24,
+                  ),
+                );
+              },
+            ),
+            const SizedBox(height: 4),
+            AnimatedDefaultTextStyle(
+              duration: const Duration(milliseconds: 200),
+              style: TextStyle(
+                color: isSelected ? primaryColor : secondaryColor,
+                fontSize: isSelected ? 12 : 11,
+                fontWeight: isSelected ? FontWeight.bold : FontWeight.normal,
+              ),
+              child: Text(
+                label,
+                textAlign: TextAlign.center,
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
+            if (isSelected)
+              AnimatedBuilder(
+                animation: _animations[index],
+                builder: (context, child) {
+                  return Container(
+                    margin: const EdgeInsets.only(top: 4),
+                    height: 3,
+                    width: 20 * _animations[index].value,
+                    decoration: BoxDecoration(
+                      color: primaryColor,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  );
+                },
+              ),
+          ],
+        ),
+      ),
     );
   }
 }
