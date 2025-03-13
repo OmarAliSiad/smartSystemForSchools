438be3fa7b6212c82b278e0d864a850c9543c3b8
omarali <omaraliabdelaziz250@gmail.com>
2025-03-13 14:44:19 +0200

    custom_allegris_widget

--- lib/features/child_details_view/widgets/custom_allgeries_widget.dart
+++ lib/features/child_details_view/widgets/custom_allgeries_widget.dart
@@ -1,5 +1,4 @@
 import 'dart:developer';
-
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -109,7 +108,7 @@ class _CustomAllergiesWidgetState extends State<CustomAllergiesWidget> {
                                   ),
                                   itemCount: allegryDetails.result!.length,
                                   itemBuilder: (context, index) {
-                                    return buildAllergyChip(
+                                    return BuildAllergyChip(
                                         name: allegryDetails
                                                 .result?[index].category?.name
                                                 .toString() ??
